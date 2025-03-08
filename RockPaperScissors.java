8f925372469604e495137c60fa9b8974768c5109
Prashant K Pathak <123973637+prashant152005@users.noreply.github.com>
2025-03-08 21:44:41 +0530

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -5,7 +5,7 @@ public class RockPaperScissors {
         
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Let's play Rock Paper Scissors.");
+        System.out.println("Let's play Rock Paper or Scissors.");
         System.out.println("When I say 'shoot', Choose: rock, paper, or scissors.\n");
         System.out.println("Are you ready? Write 'yes' if you are.");
         String ready = scan.nextLine();

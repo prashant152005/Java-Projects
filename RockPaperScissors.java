4a23638f931a1b39bc363321477d18030e2f04b0
Prashant K Pathak <123973637+prashant152005@users.noreply.github.com>
2025-03-10 22:55:17 +0530

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
-        
+         
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Let's play Rock Paper or Scissors.");
